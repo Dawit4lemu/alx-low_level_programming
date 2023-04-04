e27fe2d0f0243bbb021cc6ffbef86788c7a67b61
dawit4lemu <dawit22alemu@gmail.com>
2023-04-04 01:21:14 -0700

    0main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <_putchar.c>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,3 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif
+
